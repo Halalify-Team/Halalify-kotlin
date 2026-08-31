150f9bbba81be4f55b01acafc69e1ea146a7c6c4
awad <awad99>
2026-08-31 10:03:50 +0300

    remove extension

--- halalify-browser-extension-starter/src/core/settings.js
+++
@@ -1,6 +0,0 @@
-globalThis.HalalifySettings = {
-  async isProtectionEnabled() {
-    const { protectionEnabled } = await chrome.storage.local.get({ protectionEnabled: true });
-    return protectionEnabled;
-  }
-};
